0a165fc43a46b63faa3c87cf9e342d3a1ac8bff4
qiufuwei <qiufuwei15@gmail.com>
2026-03-04 11:52:38 +0800

    book entity and controller

---
+++ modules/demo-database-config/src/main/java/demo/databaseconfig/entity/Book.java
@@ -0,0 +1,4 @@
+package demo.databaseconfig.entity;
+
+public class Book {
+}
---
+++ src/main/java/demo/controller/BookController.java
@@ -0,0 +1,10 @@
+package demo.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BookController {
+}
